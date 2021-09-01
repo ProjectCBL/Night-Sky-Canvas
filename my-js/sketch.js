4cb8483fcfc34c0e2432e4dc43a23691ff7ca018
ProjectCBL <projectxcbl@gmail.com>
2021-08-31 23:16:01 -0400

    Created first draft of building layout

--- my-js/sketch.js
+++ my-js/sketch.js
@@ -1,36 +1,80 @@
-let width = window.innerWidth-5;
-let height = 500;
-let building1;
-let building2;
-let building3;
+let buildings = [];
+let backgroundColor = "#08141e";
+let screen = {
+    width: window.innerWidth-5,
+    height: 500
+}
+let defaultBuildingDimensions = {
+    width: 60,
+    height: 100
+};
 
 function setup() {
-    createCanvas(width, height);
-    building1 = new Building(
-        width/2,
-        height/2,
-        75,
-        150
-    );
-    building2 = new Building(
-        width/2-200,
-        height/2,
-        60,
-        100
-    );
-    building3 = new Building(
-        width/2+200,
-        height/2,
-        60,
-        100
-    );
-    /*let red = color([255,0,0,255]);
-    console.log(red.__getRed());*/
+
+    let startCoord = {
+        x: 0,
+        y: screen.height
+    }
+    const endCoord = {
+        x: screen.width, 
+        y: screen.height
+    }
+    const heightMultipliers = [
+        1.0, 1.1, 1.2, 1.3, 1.4, 1.5
+    ];
+    const widthMultipliers = [
+        0.8, 0.9, 1.0, 1.1
+    ];
+    const buildingSpace = 30;
+
+    createCanvas(screen.width, screen.height);
+
+    function getNewBuildingStructure() {
+        let indexWidth = getRandomIntFrom(widthMultipliers.length);
+        let indexHeight = getRandomIntFrom(heightMultipliers.length);
+        let newBuildingStructure = {
+            width: defaultBuildingDimensions.width * widthMultipliers[indexWidth],
+            height: defaultBuildingDimensions.height * heightMultipliers[indexHeight]
+        };
+        return newBuildingStructure;
+    }
+
+    while(true){
+        let newStructure = getNewBuildingStructure();
+
+        if (startCoord.x == 0){
+            const cutoffRanges = [0.30, 0.35, 0.40];
+            const cutoffIndex = getRandomIntFrom(cutoffRanges.length);
+            startCoord.x -= newStructure.width * cutoffRanges[cutoffIndex];
+        }
+        else if(startCoord.x > endCoord.x){
+            break;
+        }
+        else{
+            startCoord.x += 
+            (buildings[buildings.length-1].width/1.5) 
+            + buildingSpace;
+        }
+
+        buildings.push(
+            new Building(
+                startCoord.x, 
+                startCoord.y, 
+                newStructure.width, 
+                newStructure.height
+            )
+        );
+    }
+
 }
 
 function draw() {
-    background("#08141e");
-    building1.draw();
-    building2.draw();
-    building3.draw();
+    background(backgroundColor);
+    drawAllBuildings();
+}
+
+function drawAllBuildings(){
+    for (let i = 0; i < buildings.length; i++){
+        buildings[i].draw();
+    }
 }
